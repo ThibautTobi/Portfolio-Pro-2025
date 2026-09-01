a277cb88332ff1267714da3a203d2bbfb5c24ded
thibaut denis <thibaut_du_denis@hotmail.fr>
2026-09-01 15:39:21 +0200

    refactor : nettoyage de code

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./globals.css";
  * Elles permettent aux moteurs de recherche et aux plateformes
  * sociales de comprendre le contenu de ton site.
  */
+
 export const metadata: Metadata = {
     title: "Denis Thibaut | Développeur Web Junior Front-End / Full Stack",
 
@@ -52,10 +53,6 @@ export const metadata: Metadata = {
     },
 };
 
-/**
- * Layout racine de l'application.
- */
-
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -69,68 +66,3 @@ export default function RootLayout({
         </html>
     );
 }
-
-
-// import type { Metadata } from "next";
-// import localFont from "next/font/local";
-// //  a importer Playwrite India
-// // import  from "next/font/local";
-// import "../app/globals.css";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
-// export const metadata: Metadata = {
-//     title:  
-//       "Denis Thibaut — Développeur Web Junior | Front-End & Full Stack",
-//     description:
-//       "Portfolio de Denis Thibaut, développeur web junior orienté Front-End et Full Stack. Découvrez ses projets et compétences en JavaScript, TypeScript, React et Next.js.",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="fr" className="h-full w-full">
-//       <body
-//         className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
-//       >
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
-
-// exemple carte qi profil partager
-
-// openGraph: {
-//     type: "website",
-//     locale: "fr_FR",
-
-//     title:
-//         "Denis Thibaut | Développeur Web Junior Front-End / Full Stack",
-
-//     description:
-//         "Portfolio de Denis Thibaut, développeur web junior spécialisé en Front-End.",
-
-//     siteName: "Denis Thibaut — Portfolio",
-
-//     images: [
-//         {
-//             url: "/og-image.png",
-//             width: 1200,
-//             height: 630,
-//             alt: "Denis Thibaut — Développeur Web Junior",
-//         },
-//     ],
-// },
