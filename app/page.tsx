9a3f5202367b30859446876cfe211ebec0f889fd
thibaut denis <thibaut_du_denis@hotmail.fr>
2026-09-01 15:19:23 +0200

    refactor : suppresion de l'ancien code restant

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,12 @@
-//import Skills from "./components/Skills";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-//import { Hero } from "./components/Hero";
-//import { Spacing } from "./components/Spacing";
 import Presentation from "./components/Presentation";
-// import Contact from "./components/Contact";
-// import Separator from "./components/separateur";
-// import About from "./components/About";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Presentation />
-
-      {/* ancienne version */}
-
-        {/* <main>    
-          <Spacing size="mb" />
-          <Hero />
-          <Spacing size="mb" />
-
-          <Separator />
-
-          <Spacing size="mb" />
-          <About />
-          <Spacing size="mb" />
-
-          <Separator />
-
-          <Spacing size="mb" />
-          <Skills />
-          <Spacing size="mb" />
-
-          <Separator />
-
-          <Spacing size="mb" />
-          <Presentation />
-          <Spacing size="mb" />
-
-          <Separator />
-
-          <Spacing size="mb" />
-          <Contact />
-          <Spacing size="mb" />
-        </main> */}
-
       <Footer />
     </>
   );
