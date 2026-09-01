99422c7252da2461b540bda65bb016d9d7320e12
thibaut denis <thibaut_du_denis@hotmail.fr>
2026-09-01 15:25:18 +0200

    fix correction manque de l'id de section et suppresion de l'ancien code restant

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,21 +1,5 @@
 import Image from "next/image"
 import moi from "@/public/image/image-portfoliot-dev.webp"
-//import SpanText from "./SpanText"
-// import { PlaneTakeoff } from 'lucide-react';
-// import france from "@/public/image/icon-france-32.png"
-//import { GroupIcon } from "./icons/Groupicon"
-
-
-// a tester une autre facon de faire .
-// import { ComponentPropsWithoutRef } from "react"
-
-// const Code = ({ className, ...props}: ComponentPropsWithoutRef<"span">) => {
-//     return <span className={cn("bg-accent/30 border-accent p-1 -m1", className)}
-//             {...props}
-//             />
-// }
-//  corriger et importer en svg
-
 
 /**
  * Hero principal du portfolio.
@@ -32,6 +16,7 @@ export const Hero = () => {
 
     return (
         <section
+            id="home"
             aria-labelledby="hero-title"
             className="flex flex-col max-w-5xl px-4 py-16 mx-auto gap-10"
         >
@@ -100,26 +85,3 @@ export const Hero = () => {
         </section>
     )
 }
-
-
-
-// ancien contenue recherche alternance
-{/* <div className="flex-[2]">
-                <h2 className="font-caption text-4xl text-primary mb:text-5xl">Denis Thibaut</h2>
-                <h3 className="font-caption text-2xl mt-2 mb-2">Front-End · Full Stack</h3>
-                <p>Je suis <SpanText icon={france}>Thibaut</SpanText> développeur Full Stack React / Node Js.<br/>
-                   J&apos;adore travailler en <SpanText icon={GroupIcon}>équipe</SpanText> et relever de nouveaux challenges Je suis actuellement a la recherche d&apos;un emploi en Front, Back ou Full Stack.<br/>
-                    Je suis ouvert à une alternance pour un bac +3/+4 ce qui me permettra d&apos;améliorer mes compétences et <SpanText>mon stack technique<PlaneTakeoff /></SpanText>.<br/>
-                    Ceci me permettra de faire mes preuves et de pouvoir apporter ma vision et mes connaissances ainsi que d&apos;avoir une nouvelle expérience au sein d&apos;une entreprise qui sera a coup sur enrichissante.<br/>
-                    J&apos;aime relever des défis et je trouve toujours des solutions sur les problématiques que je rencontre.
-                </p>
-            </div>
-            <div className="mr-0 p-6">
-                <Image 
-                    src={moi}
-                    width={200}
-                    height={200}
-                    alt="moi"
-                    className="border-2 border-white rounded-full"
-                />
-            </div> */}
