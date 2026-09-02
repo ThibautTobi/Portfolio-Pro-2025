23c8851ede2df37dcaae1a2e7bd4f919455a128f
thibaut denis <thibaut_du_denis@hotmail.fr>
2026-09-02 14:12:19 +0200

    feat :  ajout schema.org et modification balise semantique

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,47 @@ import Header from "./components/Header";
 import Presentation from "./components/Presentation";
 
 export default function Home() {
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+
+    name: "Denis Thibaut",
+
+    jobTitle: "Développeur Web Junior",
+
+    url: "https://portfolio-pro-2025.vercel.app",
+
+    sameAs: [
+        "https://github.com/ThibautTobi",
+        "https://www.linkedin.com/in/thibaut-denis-2b12b21b1/",
+    ],
+
+    knowsAbout: [
+        "JavaScript",
+        "TypeScript",
+        "React",
+        "Next.js",
+        "Node.js",
+        "HTML",
+        "CSS",
+        "Développement web",
+    ],
+};
+
   return (
     <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+        __html: JSON.stringify(jsonLd),
+        }}
+      />
+
       <Header />
-      <Presentation />
+        <main>
+          <Presentation />
+        </main>
       <Footer />
     </>
   );
