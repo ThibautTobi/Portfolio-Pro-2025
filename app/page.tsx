ec91b32eefff94f8df6238b15ed95da42108002a
thibaut denis <thibaut_du_denis@hotmail.fr>
2026-09-15 22:17:01 +0200

    refactor : modification  schema.ORG

--- app/page.tsx
+++ app/page.tsx
@@ -3,38 +3,32 @@ import Header from "./components/Header";
 import Presentation from "./components/Presentation";
 
 export default function Home() {
-  const siteUrl = "https://portfolio-pro-2025.vercel.app";
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Person",
 
-    const jsonLd = {
-      "@context": "https://schema.org",
-      "@type": "Person",
-      "@id": `${siteUrl}/#person`,
+    name: "Denis Thibaut",
 
-      name: "Denis Thibaut",
+    jobTitle: "Développeur Web Junior",
 
-      jobTitle: "Développeur Web Junior",
+    url: "https://portfolio-pro-2025.vercel.app",
 
-      description:
-        "Développeur web junior spécialisé en Front-End avec JavaScript, TypeScript, React et Next.js, ouvert aux opportunités Front-End et Full Stack.",
+    sameAs: [
+      "https://github.com/ThibautTobi",
+      "https://www.linkedin.com/in/thibaut-denis-2b12b21b1/",
+    ],
 
-      url: siteUrl,
-
-      sameAs: [
-        "https://github.com/ThibautTobi",
-        "https://www.linkedin.com/in/thibaut-denis-2b12b21b1/",
-      ],
-
-      knowsAbout: [
-        "JavaScript",
-        "TypeScript",
-        "React",
-        "Next.js",
-        "Node.js",
-        "HTML",
-        "CSS",
-        "Développement web",
-      ],
-    };
+    knowsAbout: [
+      "JavaScript",
+      "TypeScript",
+      "React",
+      "Next.js",
+      "Node.js",
+      "HTML",
+      "CSS",
+      "Développement web",
+    ],
+  };
 
   return (
     <>
